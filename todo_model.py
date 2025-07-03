4ddd6130afd43940f7ac886b0e1fa1299cc16ece
shamrocksprings10 <ayush.sharma.se@gmail.com>
2025-07-03 11:31:12 -0500

    text truncation + todo deletion

--- todo_model.py
+++ todo_model.py
@@ -3,6 +3,10 @@ from PySide6.QtWidgets import QMessageBox, QWidget
 
 from database import TodoDB, TodoUpdate
 
+import re, string
+from textwrap import shorten as truncate
+
+TODO_CHAR_WIDTH = 60
 
 class TodoModel(QAbstractListModel):
     def __init__(self, db: TodoDB, parent: QWidget):
@@ -19,9 +23,9 @@ class TodoModel(QAbstractListModel):
 
     def data(self, index: QModelIndex, role=Qt.ItemDataRole.DisplayRole):
         if role == Qt.ItemDataRole.DisplayRole:
-            return self.todos[index.row()].content
-        # elif role == Qt.ItemDataRole.ToolTipRole:
-        #     return self.todos[index.row()].content
+            content = self.todos[index.row()].content
+            content = re.sub(f"[{string.whitespace}]]", " ", content)
+            return truncate(content, TODO_CHAR_WIDTH, placeholder="...")
         elif role == Qt.ItemDataRole.CheckStateRole:
             return int(self.todos[index.row()].completed) * 2
         return None
@@ -41,6 +45,13 @@ class TodoModel(QAbstractListModel):
             self.db.update_todo(id_, TodoUpdate(completed=checked))
             self.todos = self.refresh_list()
             return True
+
+    def delete_todo(self, index: QModelIndex):
+        self.beginRemoveRows(index, index.row(), index.row())
+        id_ = self.todos[index.row()].id
+        self.db.delete_todo(id_)
+        self.todos = self.refresh_list()
+        self.endRemoveRows()
 
     def flags(self, index: QModelIndex):
         return Qt.ItemIsEditable | Qt.ItemIsUserCheckable | super().flags(index)
