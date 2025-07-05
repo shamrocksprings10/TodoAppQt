581c1d0441cd818cf8a1a8239cc4f11ccfa154bb
shamrocksprings10 <ayush.sharma.se@gmail.com>
2025-07-05 11:15:54 -0500

    add image to readme

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # TodoAppQt
 
 Used Tango Desktop Icons for the GUI. Everything GUI-related was made using Qt with PySide6 bindings. See `requirements.txt` for more information.
+<br>
+![Image of GUI](docs/img.png)
--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from PySide6.QtGui import QIcon
 from PySide6.QtWidgets import QApplication, QMainWindow
 
 from config import Config
-from database import TodoDB
+from database import TodoDB, TodoIn
 from todo_widget import TodoWidget
 
 
@@ -24,6 +24,10 @@ if __name__ == "__main__":
     app = QApplication([])
     db = TodoDB()
     db.init_table()
+
+    if len(db.get_all_todos()) == 0:
+        db.insert_todo(TodoIn(content="Create Some Todos!", completed=0))
+
     window = MainWindow(Config["gui"]["window_title"], (800, 600), db)
     window.show()
     print(f"Exited with status code {app.exec()}")
